0cd0a4c36e68b168811c016165a8696e34fc8be5
minseok-ryu <minseok.ryu1@gmail.com>
2021-11-09 21:38:29 -0700

    incorporate image size calculation

--- appfl/models/cnn1.py
+++ appfl/models/cnn1.py
@@ -1,24 +1,33 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
-
+import math
 class CNN1(nn.Module):
-    def __init__(self, in_features, num_classes):
+    def __init__(self, in_features, num_classes, pixel):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_features, 6, 5)  ## in_channels, out_channels, kernel_size
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        # self.fc1 = nn.Linear(16 * 4 * 4, 120)  # 1*28*28 -> 16*4*4
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)    # 3*32*32 -> 16*5*5
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
+        self.conv1 = nn.Conv2d(in_features, 32, kernel_size=5, padding=0, stride=1, bias=True)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=5, padding=0, stride=1, bias=True)        
+        self.maxpool = nn.MaxPool2d(kernel_size=(2, 2))
+        self.act = nn.ReLU(inplace=True)
+        
+        ################################################################################### 
+        #### X_out = floor{ 1 + (X_in + 2*padding - dilation*(kernel_size-1) - 1)/stride }
+        ################################################################################### 
+        X = pixel
+        X = math.floor( 1+(X + 2*0 - 1*(5-1)-1)/1 )
+        X = X/2
+        X = math.floor( 1+(X + 2*0 - 1*(5-1)-1)/1 )
+        X = X/2
+        X = int(X)
+        
+        self.fc1 = nn.Linear(64*X*X, 512)           
+        self.fc2 = nn.Linear(512, num_classes)
+ 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.act(self.conv1(x))
+        x = self.maxpool(x)
+        x = self.act(self.conv2(x))
+        x = self.maxpool(x)
+        x = torch.flatten(x, 1)
+        x = self.act(self.fc1(x))
+        x = self.fc2(x)
         return x
