47d4fb68e7be6d385f5ab9aceb6fd1376c7c8655
minseok-ryu <minseok.ryu1@gmail.com>
2021-11-09 21:41:02 -0700

    gradient using all local data

--- appfl/algorithm/iadmm.py
+++ appfl/algorithm/iadmm.py
@@ -33,20 +33,10 @@ class IADMMServer(BaseServer):
     # update global model
     def update(self, global_state: OrderedDict , local_states: OrderedDict):
 
-        # print("global_state=", global_state["fc2.bias"])
-        # for i in range(self.num_clients):
-        #     print("client=", i, " local_state=", local_states[i]["fc2.bias"])
-
-        ## Update dual        
-        # for i in range(self.num_clients):            
-            # print("client=", i, " dual=", self.dual_states[i]["fc2.bias"][0], " global=", global_state["fc2.bias"][0], " local=",local_states[i]["fc2.bias"][0])
+        ## Update dual                
         for name, param in self.model.named_parameters():            
             for i in range(self.num_clients):
                 self.dual_states[i][name] = self.dual_states[i][name] + self.penalty*(global_state[name] - local_states[i][name])
-
-
-        # for i in range(self.num_clients):
-        #     print("client=", i, " dual_next=", self.dual_states[i]["fc2.bias"])
 
         ## Update global
         for name, param in self.model.named_parameters():
@@ -115,48 +105,34 @@ class IADMMClient(BaseClient):
 
         ## Global state
         for name, param in self.model.named_parameters():
-            self.global_state[name] = copy.deepcopy(param.data)
-
-        # print("Sub: id=", self.id, " global_state=", self.global_state["fc2.bias"]) 
-        # print("Sub: id=", self.id, " local_state=", self.local_state["fc2.bias"]) 
+            self.global_state[name] = copy.deepcopy(param.data)        
         
-
         for _ in range(self.num_local_epochs):
+            
             ## Gradient of the local point
             self.model.load_state_dict(self.local_state)
             for name, param in self.model.named_parameters():            
                 self.local_grad[name] = torch.zeros_like(param.data)                
-            tmpcnt=0
-            for data, target in self.dataloader:                
-                tmpcnt+=1
+             
+            for data, target in self.dataloader:                                
                 data = data.to(self.device)
                 target = target.to(self.device)                                    
                 output = self.model(data)
-                loss = self.loss_fn(output, target)
+                loss = self.loss_fn(output, target)                
                 loss.backward()            
-                
+             
                 for name, param in self.model.named_parameters():
-                    self.local_grad[name] += param.grad
-    
-            # print("Sub: id=", self.id, " local_grad=", self.local_grad["fc2.bias"])
-    
+                    self.local_grad[name] = param.grad
+            
             ## Update local
             for name, param in self.model.named_parameters():
                 self.local_state[name] = self.global_state[name] + (1.0/self.penalty) * ( self.dual_state[name] - self.local_grad[name] )
         
-        
-        
-        
-        ## Update dual
-        # print("Sub: id=", self.id, " dual=", self.dual_state["fc2.bias"][0], " global=", self.global_state["fc2.bias"][0], " local=",self.local_state["fc2.bias"][0])
+        ## Update dual        
         for name, param in self.model.named_parameters():
             self.dual_state[name] = self.dual_state[name] + self.penalty*(self.global_state[name] - self.local_state[name])
         
-        # print("Sub: id=", self.id, " dual_next=", self.dual_state["fc2.bias"])
-
         ## Update model
         self.model.load_state_dict(self.local_state)
 
-        # print("Sub: id=", self.id, " next_local_state=", self.local_state["fc2.bias"])  
-        # return self.local_state 
         
