3c46abc1dc7eec9b9be018522bcbaac38b6dd80f
mryu <minseok.ryu1@gmail.com>
2021-11-11 10:36:33 -0600

    GPU

--- appfl/run.sh
+++ appfl/run.sh
@@ -3,11 +3,13 @@
 #SBATCH --job-name=mryu
 #SBATCH --account=NEXTGENOPT
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:1
-#SBATCH -w, --nodelist=gpu2
-#SBATCH --time=15:00:00
+#SBATCH --gres=gpu:5
+#SBATCH --time=1:00:00
 #SBATCH --array=1
 #SBATCH --output=./Slurm_outputs/Slurm_outputs_run_%a.out
 CASE_NUM=`printf %1d $SLURM_ARRAY_TASK_ID`
-export I_MPI_FABRICS=shm:tmi
-mpiexec -np 3 python ./run1.py
+
+mpiexec -np 5 --mca opal_cuda_support 1 python ./run.py dataset=mnist model=cnn1 fed=fedavg
+mpiexec -np 5 --mca opal_cuda_support 1 python ./run.py dataset=mnist model=cnn1 fed=iadmm
+mpiexec -np 5 --mca opal_cuda_support 1 python ./run.py dataset=cifar10 model=cnn1 fed=fedavg
+mpiexec -np 5 --mca opal_cuda_support 1 python ./run.py dataset=cifar10 model=cnn1 fed=iadmm
