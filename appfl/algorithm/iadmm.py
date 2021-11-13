a3fe79883bf33a83935bb600cc193c17a655b804
minseok-ryu <minseok.ryu1@gmail.com>
2021-11-13 13:59:52 -0700

    Update Validation

--- appfl/algorithm/iadmm.py
+++ appfl/algorithm/iadmm.py
@@ -56,19 +56,22 @@ class IADMMServer(BaseServer):
         self.model.eval()
         test_loss = 0
         correct = 0
+        tmpcnt=0; tmptotal=0
         with torch.no_grad():
             for img, target in self.dataloader:
+                tmpcnt+=1; tmptotal+=len(target)
                 img = img.to(self.device)
                 target = target.to(self.device)
-                logits = self.model(img)
+                logits = self.model(img)                
                 test_loss += self.loss_fn(logits, target).item()
                 pred = logits.argmax(dim=1, keepdim=True)
                 correct += pred.eq(target.view_as(pred)).sum().item()
 
         # FIXME: do we need to sent the model to cpu again?
         # self.model.to("cpu")
-        test_loss = test_loss / len(self.dataloader)
-        accuracy = 100.0 * correct / len(self.dataloader.dataset)
+        
+        test_loss = test_loss / tmpcnt
+        accuracy = 100.0 * correct / tmptotal
 
         return test_loss, accuracy
 
