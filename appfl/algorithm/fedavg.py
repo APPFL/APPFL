76ff92371a4a45e47ba7a33eae8ad8542645c8a3
minseok-ryu <minseok.ryu1@gmail.com>
2021-11-13 13:59:52 -0700

    Update Validation

--- appfl/algorithm/fedavg.py
+++ appfl/algorithm/fedavg.py
@@ -34,7 +34,34 @@ class FedAvgServer(BaseServer):
                     update_state[key] = state[key] / self.num_clients
 
         self.model.load_state_dict(update_state)
+    
+    # NOTE: this is only for testing purpose.
+    def validation(self):
+        if self.loss_fn is None or self.dataloader is None:
+            return 0.0, 0.0
 
+        self.model.to(self.device)
+        self.model.eval()
+        test_loss = 0
+        correct = 0
+        tmpcnt=0; tmptotal=0
+        with torch.no_grad():
+            for img, target in self.dataloader:
+                tmpcnt+=1; tmptotal+=len(target)
+                img = img.to(self.device)
+                target = target.to(self.device)
+                logits = self.model(img)                
+                test_loss += self.loss_fn(logits, target).item()
+                pred = logits.argmax(dim=1, keepdim=True)
+                correct += pred.eq(target.view_as(pred)).sum().item()
+
+        # FIXME: do we need to sent the model to cpu again?
+        # self.model.to("cpu")
+        
+        test_loss = test_loss / tmpcnt
+        accuracy = 100.0 * correct / tmptotal
+
+        return test_loss, accuracy
 
 
 class FedAvgClient(BaseClient):
@@ -55,7 +82,7 @@ class FedAvgClient(BaseClient):
         optimizer = self.optimizer(self.model.parameters(), **self.optimizer_args)
  
         for i in range(self.num_local_epochs):
-            log.info(f"[Client ID: {self.id: 03}, Local epoch: {i+1: 04}]")
+            # log.info(f"[Client ID: {self.id: 03}, Local epoch: {i+1: 04}]")
             
             for data, target in self.dataloader:                
                 data = data.to(self.device)
