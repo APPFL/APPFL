e8f8c774a0e7235f5489228e314a9f7b02efd06f
mryu <minseok.ryu1@gmail.com>
2021-11-24 13:11:28 -0600

    Test on GPU

--- examples/run.sh
+++ examples/run.sh
@@ -3,10 +3,17 @@
 #SBATCH --job-name=mryu
 #SBATCH --account=NEXTGENOPT
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:5
+#SBATCH --gres=gpu:2
 #SBATCH --time=1:00:00
 #SBATCH --array=1
-#SBATCH --output=./Slurm_outputs/Slurm_outputs_run_%a.out
+#SBATCH --output=../Slurm_outputs/Slurm_outputs_run_%a.out
 CASE_NUM=`printf %1d $SLURM_ARRAY_TASK_ID`
 
-mpiexec -np 5 --mca opal_cuda_support 1 python ./run.py dataset=femnist model=cnn1 fed=iadmm
+mpiexec -np 2 --mca opal_cuda_support 1 python ./mnist.py fed=fedavg num_epochs=1 batch_size=64 device=cuda
+mpiexec -np 2 --mca opal_cuda_support 1 python ./mnist.py fed=iadmm num_epochs=1 batch_size=64 device=cuda
+mpiexec -np 2 --mca opal_cuda_support 1 python ./femnist.py fed=fedavg num_epochs=1 batch_size=64 device=cuda
+mpiexec -np 2 --mca opal_cuda_support 1 python ./femnist.py fed=iadmm num_epochs=1 batch_size=64 device=cuda
+mpiexec -np 2 --mca opal_cuda_support 1 python ./coronahack.py fed=fedavg num_epochs=1 batch_size=64 device=cuda
+mpiexec -np 2 --mca opal_cuda_support 1 python ./coronahack.py fed=iadmm num_epochs=1 batch_size=64 device=cuda
+mpiexec -np 2 --mca opal_cuda_support 1 python ./cifar10.py fed=fedavg num_epochs=1 batch_size=64 device=cuda
+mpiexec -np 2 --mca opal_cuda_support 1 python ./cifar10.py fed=iadmm num_epochs=1 batch_size=64 device=cuda
