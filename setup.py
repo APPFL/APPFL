d50959d786f8aed36c2dd69ba40a96a8e6aaedcc
Kibaek Kim <kibaek.k@gmail.com>
2022-01-31 22:23:14 -0600

    configure setuptools

---
+++ setup.py
@@ -0,0 +1,65 @@
+import setuptools
+
+# Build Author list
+authors = {
+    "Kibaek Kim": "kimk@anl.gov",
+    "Minseok Ryu": "mryu@anl.gov",
+    "Youngdae Kim": "youngdae@anl.gov",
+    "Mark Xiao": "ymxiao@anl.gov",
+}
+AUTHOR = ""
+for i, (k, v) in enumerate(authors.items()):
+    if i > 0:
+        AUTHOR += ", "
+    AUTHOR += f"{k} <{v}>"
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="appfl",
+    version="0.0.1",
+    author=AUTHOR,
+    description="An open-source package for privacy-preserving federated learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/APPFL/APPFL",
+    project_urls={
+        "Bug Tracker": "https://github.com/APPFL/APPFL/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy",
+        'torch >= 1.9.1',
+        'hydra-core',
+        'mpi4py',
+    ],
+    extras_require={
+        "dev": [
+            "sphinx",
+            "sphinx-book-theme",
+            "myst-parser",
+            "twine",
+            "build",
+            "black",
+            "pytest",
+        ],
+        "analytics": [
+            "jupyter",
+            "tensorboard",
+            "matplotlib",
+        ],
+        "examples": [
+            "opencv-python",
+            "torchvision",
+        ],
+    },
+)
