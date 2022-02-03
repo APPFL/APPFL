72cb31f1eb8e450f75c4a516ce91965f3d39fee4
mryu <minseok.ryu1@gmail.com>
2022-02-03 00:28:03 -0600

    Output Penalty (min and max values)

--- src/appfl/run.py
+++ src/appfl/run.py
@@ -199,7 +199,7 @@ def run_server(
         LocalUpdate_time = time.time() - LocalUpdate_start
 
         GlobalUpdate_start = time.time()        
-        prim_res, dual_res = server.update(local_states)
+        prim_res, dual_res, rho_min, rho_max = server.update(local_states)
         GlobalUpdate_time = time.time() - GlobalUpdate_start
 
         Validation_start = time.time()
@@ -222,7 +222,9 @@ def run_server(
             test_loss,
             accuracy,
             prim_res, 
-            dual_res
+            dual_res,
+            rho_min, 
+            rho_max,
         )
 
         if np.isnan(test_loss) == True:
