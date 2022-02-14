1d1caad3c82602b9cecbadef5a00606eac402be2
Kibaek Kim <kibaek.k@gmail.com>
2022-02-14 11:33:43 -0600

    initialize fed_server early

--- src/appfl/protos/operator.py
+++ src/appfl/protos/operator.py
@@ -38,7 +38,8 @@ class FLOperator():
                                      num_workers=0,
                                      batch_size=cfg.test_data_batch_size,
                                      shuffle=cfg.test_data_shuffle)
-        self.fed_server = None
+        self.fed_server = eval(self.cfg.fed.servername)(
+            self.client_weights, self.model, self.num_clients, self.device, **self.cfg.fed.args)
 
     """
     Return the tensor record of a global model requested by its name.
@@ -70,9 +71,7 @@ class FLOperator():
             total_training_size = sum(self.client_training_size[c] for c in range(self.num_clients))
             for c in range(self.num_clients):
                 self.client_weights[c] = self.client_training_size[c] / total_training_size
-            if self.fed_server is None:
-                self.fed_server = eval(self.cfg.fed.servername)(
-                    self.client_weights, self.model, self.num_clients, self.device, **self.cfg.fed.args)
+            self.fed_server.weights = self.client_weights
             return self.client_weights[client_id]
         else:
             return -1.0
