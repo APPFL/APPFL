db6e5bae56f6c41c245320cd180225e719311d35
mryu <minseok.ryu1@gmail.com>
2022-03-11 12:01:33 -0600

    renaming filename

--- src/appfl/algorithm/__init__.py
+++ src/appfl/algorithm/__init__.py
@@ -2,15 +2,15 @@
 This module provides a base class to define the functions required to implement PPFL algorithms.
 """
 
-from .Client_sgd import *
-from .Server_fed_avg import *
-from .Server_fed_avgmom import *
-from .Server_fed_adagrad import *
-from .Server_fed_adam import *
-from .Server_fed_yogi import *
-from .Server_fed_bfgs import *
-from .Server_fed_broyden import *
-from .Server_fed_dfp import *
-from .Server_fed_sr1 import *
+from .client_sgd import *
+from .server_fed_avg import *
+from .server_fed_avgmom import *
+from .server_fed_adagrad import *
+from .server_fed_adam import *
+from .server_fed_yogi import *
+from .server_fed_bfgs import *
+from .server_fed_broyden import *
+from .server_fed_dfp import *
+from .server_fed_sr1 import *
 from .iceadmm import *
 from .iiadmm import *
