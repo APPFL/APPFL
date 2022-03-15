55d00bf382d1b188b8051c86311bf10614937fa4
minseok-ryu <minseok.ryu1@gmail.com>
2022-03-15 11:19:02 -0700

    update the deepcovid example

--- examples/deepcovid.py
+++ examples/deepcovid.py
@@ -20,19 +20,13 @@ num_clients = 1
 num_channel = 3  # 1 if gray, 3 if color
 num_classes = 2  # number of the image classes
 num_pixel = 32  # image size = (num_pixel, num_pixel)
-
-
-def get_model(comm: MPI.Comm):
-    ## User-defined model
-    model = CNN(num_channel, num_classes, num_pixel)
-    return model
-
+ 
 
 ## Run
 def main():
     # read default configuration
     cfg = OmegaConf.structured(Config)
-    
+
     comm = MPI.COMM_WORLD
     comm_rank = comm.Get_rank()
     comm_size = comm.Get_size()
@@ -43,23 +37,7 @@ def main():
 
     start_time = time.time()
 
-    # train data     
-    with open("./datasets/PreprocessedData/deepcovid_train_data.json") as f:
-        train_data_raw = json.load(f)
     
-    
-    train_datasets = []
-    train_datasets.append(Dataset( torch.FloatTensor(train_data_raw["x"]), torch.tensor(train_data_raw["y"]) ))
-        
-    # test data 
-    with open("./datasets/PreprocessedData/deepcovid_test_data.json") as f:
-        test_data_raw = json.load(f)
-     
-    test_dataset = Dataset( torch.FloatTensor(test_data_raw["x"]), torch.tensor(test_data_raw["y"]) )
-    
- 
-    # model = get_model(comm)
-
     """ Isabelle's DenseNet (the outputs of the model are probabilities of 1 class ) """
     import imp
     MainModel = imp.load_source('MainModel', "./models/IsabelleTorch.py")
@@ -67,12 +45,28 @@ def main():
     model = torch.load(file)        
     model.eval()
     cfg.fed.args.loss_type = "torch.nn.BCELoss()"
+
+
+    """ Load Data """
+    with open("./datasets/PreprocessedData/deepcovid_train_data.json") as f:
+        train_data_raw = json.load(f)
+    
+    
+    train_datasets = []
+    train_datasets.append(Dataset( torch.FloatTensor(train_data_raw["x"]), torch.FloatTensor(train_data_raw["y"]).reshape(-1,1) ))
+        
+    # test data 
+    with open("./datasets/PreprocessedData/deepcovid_test_data.json") as f:
+        test_data_raw = json.load(f)
+     
+    test_dataset = Dataset( torch.FloatTensor(test_data_raw["x"]), torch.FloatTensor(test_data_raw["y"]).reshape(-1,1) )
+
+ 
+
     print(
         "----------Loaded Datasets and Model----------Elapsed Time=",
         time.time() - start_time,
-    )
-
-    
+    ) 
 
     if comm_size > 1:
         if comm_rank == 0:
