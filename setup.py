01f5b1e0b61d556866b284943085ac130dfedf65
Kibaek Kim <kibaek.k@gmail.com>
2022-03-17 17:35:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="appfl",
-    version="0.1.0",
+    version="0.1.1",
     author=AUTHOR,
     description="An open-source package for privacy-preserving federated learning",
     long_description=long_description,
