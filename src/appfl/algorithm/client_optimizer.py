ced9144a5e84ec455277502b716e81244fe98ca2
minseok-ryu <minseok.ryu1@gmail.com>
2022-03-25 14:31:47 -0700

    clean up

--- src/appfl/algorithm/client_optimizer.py
+++ src/appfl/algorithm/client_optimizer.py
@@ -20,8 +20,6 @@ class ClientOptim(BaseClient):
 
         self.loss_fn = eval(self.loss_type)
 
-        self.train_loss = []
-
     def update(self):
 
         self.model.train()
@@ -35,35 +33,14 @@ class ClientOptim(BaseClient):
 
         """ Multiple local update """
         for i in range(self.num_local_epochs):
-            # train_loss = 0
-            # tmpcnt = 0
             for data, target in self.dataloader:
-
                 data = data.to(self.device)
                 target = target.to(self.device)
-
                 optimizer.zero_grad()
                 output = self.model(data)
                 loss = self.loss_fn(output, target)
-                loss.backward() 
+                loss.backward()
                 optimizer.step()
-
-                # output = self.model(data)
-                
-                # loss = self.loss_fn(output, target)
-
-                # ## TODO: different functions take different formats of arguments
-                # if self.loss_type == "torch.nn.BCELoss()":
-                #     target = target.to(torch.float32)
-                #     loss = self.loss_fn(output, target.reshape(-1, 1))
-                # else:
-                #     loss = self.loss_fn(output, target)
-
-                # optimizer.zero_grad()
-                # loss.backward()
-
-                # train_loss += loss.item()
-                # tmpcnt += 1
 
                 if self.clip_value != False:
                     torch.nn.utils.clip_grad_norm_(
@@ -71,12 +48,6 @@ class ClientOptim(BaseClient):
                         self.clip_value,
                         norm_type=self.clip_norm,
                     )
-
-                # optimizer.step()
-
-            # train_loss = train_loss / tmpcnt
-
-        # self.train_loss.append(train_loss)
 
         self.primal_state = copy.deepcopy(self.model.state_dict())
 
