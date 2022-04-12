21ce036ccecc60aba55e19eeeb29430487411c6d
mryu <minseok.ryu1@gmail.com>
2022-04-12 17:17:27 -0500

    cifar10_trial

--- examples/cifar10.py
+++ examples/cifar10.py
@@ -5,80 +5,97 @@ import numpy as np
 import torch
 
 import torchvision
-from torchvision.transforms import ToTensor
+import torchvision.transforms as transforms
 
 from appfl.config import *
 from appfl.misc.data import *
 from appfl.misc.utils import *
-from models.cnn import *
 
+from models.cnn import *
+from models.resnet import *
 import appfl.run as rt
 from mpi4py import MPI
 
 import argparse
 
 DataSet_name = "CIFAR10"
-num_clients = 4
+num_clients = 1
 num_channel = 3  # 1 if gray, 3 if color
 num_classes = 10  # number of the image classes
 num_pixel = 32   # image size = (num_pixel, num_pixel)
 
 dir = os.getcwd() + "/datasets/RawData"
 
+""" read arguments """
+parser = argparse.ArgumentParser()
+parser.add_argument('--server', type=str, default="ServerFedAvg")    
+parser.add_argument('--num_epochs', type=int, default=2)    
+
+parser.add_argument('--local_optimizer', type=str, default="Adam")       
+parser.add_argument('--num_local_epochs', type=int, default=1)       
+parser.add_argument('--local_lr', type=float, default=1e-3)    
+ 
+parser.add_argument('--num_workers', type=int, default=32)  
+     
+## optional: federated adaptive
+parser.add_argument('--server_lr', type=float, required=False)    
+parser.add_argument('--mparam_1', type=float, required=False)    
+parser.add_argument('--mparam_2', type=float, required=False)    
+parser.add_argument('--adapt_param', type=float, required=False)    
+
+args = parser.parse_args()    
+
+# """ reproducibility """
+# set_seed(1)
+  
 def get_data(comm: MPI.Comm):
     comm_rank = comm.Get_rank()
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
     # Root download the data if not already available.
     if comm_rank == 0:
         # test data for a server
-        test_data_raw = eval("torchvision.datasets." + DataSet_name)(
-            dir, download=True, train=False, transform=ToTensor()
+        test_dataset = eval("torchvision.datasets." + DataSet_name)(
+            dir, download=True, train=False, transform=transforms.Compose([
+                        transforms.ToTensor(),
+                        # normalize,
+                    ])
         )
 
     comm.Barrier()
     if comm_rank > 0:
         # test data for a server
-        test_data_raw = eval("torchvision.datasets." + DataSet_name)(
-            dir, download=False, train=False, transform=ToTensor()
+        test_dataset = eval("torchvision.datasets." + DataSet_name)(
+            dir, download=False, train=False, transform=transforms.Compose([
+                        transforms.ToTensor(),
+                        # normalize,
+                    ])
         )
-
-    test_data_input = []
-    test_data_label = []
-    for idx in range(len(test_data_raw)):
-        test_data_input.append(test_data_raw[idx][0].tolist())
-        test_data_label.append(test_data_raw[idx][1])
-
-    test_dataset = Dataset(
-        torch.FloatTensor(test_data_input), torch.tensor(test_data_label)
-    )
+ 
+ 
 
     # training data for multiple clients
-    train_data_raw = eval("torchvision.datasets." + DataSet_name)(
-        dir, download=False, train=True, transform=ToTensor()
+    train_dataset = eval("torchvision.datasets." + DataSet_name)(
+        dir, download=False, train=True, transform=transforms.Compose([
+                        transforms.ToTensor(),
+                        # transforms.RandomHorizontalFlip(),
+                        # transforms.RandomCrop(32, 4),                        
+                        # normalize,
+                    ])
     )
+ 
 
-    split_train_data_raw = np.array_split(range(len(train_data_raw)), num_clients)
+    split_train_data_raw = np.array_split(range(len(train_dataset)), num_clients)        
     train_datasets = []
     for i in range(num_clients):
-
-        train_data_input = []
-        train_data_label = []
-        for idx in split_train_data_raw[i]:
-            train_data_input.append(train_data_raw[idx][0].tolist())
-            train_data_label.append(train_data_raw[idx][1])
-
-        train_datasets.append(
-            Dataset(
-                torch.FloatTensor(train_data_input),
-                torch.tensor(train_data_label),
-            )
-        ) 
-    return train_datasets, test_dataset
-
+        train_datasets.append(torch.utils.data.Subset(train_dataset, split_train_data_raw[i]))
+ 
+    return train_datasets, test_dataset 
 
 def get_model(comm: MPI.Comm):
     ## User-defined model
-    model = CNN(num_channel, num_classes, num_pixel)
+    # model = CNN(num_channel, num_classes, num_pixel)
+    model = resnet20(num_classes=num_classes)    
     return model
 
 
@@ -88,29 +105,21 @@ def main():
     comm = MPI.COMM_WORLD
     comm_rank = comm.Get_rank()
     comm_size = comm.Get_size()
-
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--server', type=str, required=True)    
-    parser.add_argument('--num_epochs', type=int, required=True)    
-    parser.add_argument('--client_lr', type=float, required=True)    
-
-    parser.add_argument('--server_lr', type=float, required=False)    
-    parser.add_argument('--mparam_1', type=float, required=False)    
-    parser.add_argument('--mparam_2', type=float, required=False)    
-    parser.add_argument('--adapt_param', type=float, required=False)    
-
-
-    args = parser.parse_args()    
+ 
 
     """ Configuration """     
     cfg = OmegaConf.structured(Config) 
     cfg.device = "cuda"
     cfg.fed.servername = args.server
     cfg.num_epochs = args.num_epochs
-    cfg.fed.args.optim_args.lr = args.client_lr
 
-    cfg.output_filename += "_%s_%s_ClientLR_%s" %(DataSet_name, args.server, args.client_lr)
+    cfg.fed.args.optim = args.local_optimizer
+    cfg.fed.args.num_local_epochs = args.num_local_epochs
+    cfg.fed.args.optim_args.lr = args.local_lr
+
+    cfg.num_workers = args.num_workers
+
+    cfg.output_filename += "_%s_%s_%s_LocalLR_%s_LocalEpochs_%s" %(DataSet_name, args.server, args.local_optimizer, args.local_lr, args.num_local_epochs)
     
     if args.server_lr != None:
         cfg.fed.args.server_learning_rate = args.server_lr
@@ -128,15 +137,10 @@ def main():
         cfg.fed.args.server_momentum_param_2 = args.mparam_2  
         cfg.output_filename += "_MParam2_%s" %(args.mparam_2)
         
-        
-    ## Reproducibility
-    if cfg.reproduce == True:
-        torch.manual_seed(1)
-        torch.backends.cudnn.deterministic = True 
-
+         
     start_time = time.time()
 
-    """ User-defined model """    
+    """ Model """    
     model = get_model(comm)
     cfg.fed.args.loss_type = "torch.nn.CrossEntropyLoss()"
     
@@ -147,15 +151,20 @@ def main():
         cfg.load_model_filename     = "Model"               
         model = load_model(cfg)         
     
-    """ User-defined data """
+    """ Data """
     train_datasets, test_dataset = get_data(comm)
 
     ## Sanity check for the user-defined data
     if cfg.data_sanity == True:
         data_sanity_check(train_datasets, test_dataset, num_channel, num_pixel)        
 
+    """ Optimizer """
+    if args.local_optimizer == "SGD":
+        cfg.fed.args.optim_args.momentum = 0.9        
+        cfg.fed.args.optim_args.weight_decay = 1e-5      
+
     print(
-        "--------Data and Model: Loading_Time=",
+        "--------Loading_Time=",
         time.time() - start_time,
     ) 
     
@@ -164,7 +173,8 @@ def main():
     if cfg.save_model == True:
         cfg.save_model_dirname      = "./save_models"
         cfg.save_model_filename     = "Model"      
-        cfg.save_model_checkpoints  = [2]
+        cfg.checkpoints_interval    = 2
+        
     
     cfg.summary_file = cfg.output_dirname + "/Summary_%s.txt" %(DataSet_name)
  
---
+++ examples/cifar10_resnet.py
@@ -0,0 +1,196 @@
+import sys
+
+from models.cnn import *
+from models.resnet import *
+from models.utils import *
+
+import argparse
+import copy 
+import torch
+import torch.optim as optim
+
+import time
+import os
+from torch.utils.data import DataLoader
+import torchvision
+import torchvision.transforms as transforms
+import logging
+  
+import numpy as np
+
+from appfl.config import *
+from appfl.misc.data import *
+from appfl.misc.utils import *
+import appfl.run as rt
+
+from mpi4py import MPI
+
+""" read arguments """
+ 
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--optimizer', type=str, required=True)   
+parser.add_argument('--num_epochs', type=int, required=True)  
+parser.add_argument('--LR', type=float, required=True)  
+
+parser.add_argument("--num_clients", type=int, default=1)
+parser.add_argument("--num_pixel", type=int, default=32)
+parser.add_argument('--num_workers', type=int, default=32)  
+
+args = parser.parse_args()    
+
+args.device = "cuda" 
+args.dataset_name = "CIFAR10"
+args.num_classes = 10
+args.loss_fn = "torch.nn.CrossEntropyLoss()"              
+
+""" output """
+name="%s_%s_LR_%s" %(args.dataset_name, args.optimizer,args.LR)
+
+args.output_dirname = "./%s/" %(name)
+args.output_filename = "summary"
+
+logger = logging.getLogger(__name__)
+logger = create_custom_logger(logger, args)
+
+
+def get_data(args):
+
+    dir = os.getcwd() + "/datasets/RawData"
+
+    # train_dataset = eval("torchvision.datasets." + args.dataset_name)(
+    #         dir, download=True, train=True, 
+    #         transform=transforms.Compose([                        
+    #                     transforms.ToTensor(),                        
+    #                 ])
+    # )
+ 
+
+    # test_dataset = eval("torchvision.datasets." + args.dataset_name)(
+    #     dir, download=True, train=False,  transform=transforms.Compose([
+    #                 transforms.ToTensor(),                    
+    #             ])
+    # ) 
+  
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    train_dataset = eval("torchvision.datasets." + args.dataset_name)(
+            dir, download=True, train=True, 
+            transform=transforms.Compose([
+                        transforms.RandomHorizontalFlip(),
+                        transforms.RandomCrop(32, 4),
+                        transforms.ToTensor(),
+                        normalize,
+                    ])
+        )
+ 
+
+    test_dataset = eval("torchvision.datasets." + args.dataset_name)(
+        dir, download=True, train=False,  transform=transforms.Compose([
+                    transforms.ToTensor(),
+                    normalize,
+                ])
+    ) 
+
+    split_train_data_raw = np.array_split(range(len(train_dataset)), args.num_clients)        
+    train_datasets = []
+    for i in range(args.num_clients):
+        train_datasets.append(torch.utils.data.Subset(train_dataset, split_train_data_raw[i]))
+            
+    return train_datasets, test_dataset
+
+def main():
+    comm = MPI.COMM_WORLD
+    comm_rank = comm.Get_rank()
+    comm_size = comm.Get_size()
+ 
+
+
+    # """ Configuration """     
+    cfg = OmegaConf.structured(Config)     
+    cfg.device = args.device      
+    cfg.output_filename += "_cifar10_%s_LR_%s_npixel_%s" %(args.optimizer, args.LR, args.num_pixel)
+
+
+    start_time = time.time()
+    """ data """ 
+    train_datasets, test_dataset = get_data(args)
+
+    """ model """ 
+    model = resnet20(num_classes=args.num_classes)  
+    # model = CNN(3, 10, 32)  
+    model.to(args.device)    
+    cfg.fed.args.loss_type = args.loss_fn
+
+    """ Optimizer """
+    cfg.fed.servername = "ServerFedAvg"
+    cfg.num_epochs = args.num_epochs    
+
+    cfg.fed.args.optim = args.optimizer
+    cfg.fed.args.num_local_epochs = 5        
+    cfg.fed.args.optim_args.lr = args.LR
+     
+    loading_time = round( time.time() - start_time, 2)
+    logger.info("Loading Time = %s" %(loading_time))
+
+    # # """ Running """     
+    # if comm_size > 1:
+    #     if comm_rank == 0:
+    #         rt.run_server(cfg, comm, model, args.num_clients, test_dataset, args.dataset_name)
+    #     else:
+    #         rt.run_client(cfg, comm, model, args.num_clients, train_datasets)
+    #     print("------DONE------", comm_rank)
+    # else:
+    #     rt.run_serial(cfg, model, train_datasets, test_dataset, args.dataset_name)
+   
+    """ Local Training """ 
+    if args.optimizer=="SGD":
+        optimizer = optim.SGD(model.parameters(), lr=args.LR, momentum=0.9, weight_decay=1e-4)     
+    
+    if args.optimizer=="Adam":
+        optimizer = optim.Adam(model.parameters(), lr=args.LR, weight_decay=0)
+
+    train_dataloader = torch.utils.data.DataLoader(
+                    train_datasets[0],
+                    batch_size=64, shuffle=True,
+                    num_workers=32, pin_memory=True)
+    test_dataloader = DataLoader(
+                test_dataset,
+                num_workers=32,
+                batch_size=64,
+                shuffle=False,
+            )     
+                                
+    """ Initial Parameter """
+    valid_stime = time.time()            
+    train_loss, train_accuracy = local_validation(args, copy.deepcopy(model), train_dataloader)
+    test_loss, test_accuracy = local_validation(args, copy.deepcopy(model), test_dataloader)
+    valid_etime = time.time() - valid_stime
+    logging_initial(logger, -1, 0.0, valid_etime, 0.0, train_loss, train_accuracy, train_auc, test_loss, test_accuracy, test_auc)  
+    
+    start_time = time.time()
+    model.train()    
+    for t in range(args.num_epochs):
+        train_stime = time.time()
+        for data, target in train_dataloader:
+            data = data.to(args.device)
+            target = target.to(args.device)
+            optimizer.zero_grad()
+            output = model(data)
+            loss = eval(args.loss_fn)(output, target)
+            loss.backward() 
+            optimizer.step()
+ 
+        train_etime = round(time.time() - train_stime,2)
+
+        valid_stime = time.time()            
+        train_loss, train_accuracy, train_auc = local_validation(args, copy.deepcopy(model), train_dataloader)            
+        test_loss, test_accuracy, test_auc = local_validation(args, copy.deepcopy(model),  test_dataloader)
+        valid_etime = time.time() - valid_stime
+
+        elapsed_time = round(time.time()-start_time,2)
+
+        logging_iteration(logger, t, train_etime, valid_etime, elapsed_time, train_loss, train_accuracy, train_auc, test_loss, test_accuracy, test_auc)
+
+
+if __name__ == "__main__":
+    main()            
