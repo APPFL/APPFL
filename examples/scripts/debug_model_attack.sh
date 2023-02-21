8596c357b6dc6adced2d7c6a791456387ba76a30
hthieu166 <hthieu@selab.hcmus.edu.vn>
2023-02-21 00:33:48 -0600

    update attack pipeline+cfg file

--- examples/scripts/debug_model_attack.sh
+++ examples/scripts/debug_model_attack.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 python funcx_debug.py \
     --client_config configs/clients/covid19newsplit2_anl.yaml \
-    --config configs/fed_avg/funcx_fedavg_eqweight_covid.yaml \
+    --config configs/model_attack/funcx_fedavg_covid_no_laplace_1epoch.yaml \
     --clients-privacy-attack \
-    --load-model \
-    --load-model-dirname log_funcx_appfl/server/outputs_CovidDataset_ServerFedAvg_Adam_funcx_fedavg_covid_covid19_anl_uchicago \
-    --load-model-filename checkpoint_30
+    # --load-model \
+    # --load-model-dirname log_funcx_appfl/server/outputs_CovidDataset_ServerFedAvg_Adam_funcx_fedavg_covid_covid19_anl_uchicago \
+    # --load-model-filename checkpoint_30
