7c372438b4d15d14530bf796f0ad5e2b95a9fefd
Sam <yim@DESKTOP-2OLE37A.localdomain>
2023-03-16 13:25:54 -0500

    add argument parser and print configuration

--- examples/femnist.py
+++ examples/femnist.py
@@ -11,17 +11,50 @@ from models.cnn import *
 import appfl.run_serial as rs
 import appfl.run_mpi as rm
 from mpi4py import MPI
+from models.utils import get_model
 
-DataSet_name = "FEMNIST"
-num_channel = 1  # 1 if gray, 3 if color
-num_classes = 62  # number of the image classes
-num_pixel = 28  # image size = (num_pixel, num_pixel)
+import argparse
 
-dir = os.getcwd() + "/datasets/RawData/%s" % (DataSet_name)
+""" read arguments """
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--device", type=str, default="cpu")
+
+## dataset
+parser.add_argument("--dataset", type=str, default="FEMNIST")
+parser.add_argument("--num_channel", type=int, default=1)
+parser.add_argument("--num_classes", type=int, default=62)
+parser.add_argument("--num_pixel", type=int, default=28)
+parser.add_argument("--model", type=str, default="CNN")
+
+## clients
+parser.add_argument("--num_clients", type=int, default=1)
+parser.add_argument("--client_optimizer", type=str, default="Adam")
+parser.add_argument("--client_lr", type=float, default=1e-3)
+parser.add_argument("--num_local_epochs", type=int, default=1)
+
+## server
+parser.add_argument("--server", type=str, default="ServerFedAvg")
+parser.add_argument("--num_epochs", type=int, default=2)
+
+parser.add_argument("--server_lr", type=float, required=False)
+parser.add_argument("--mparam_1", type=float, required=False)
+parser.add_argument("--mparam_2", type=float, required=False)
+parser.add_argument("--adapt_param", type=float, required=False)
+
+
+args = parser.parse_args()
+
+if torch.cuda.is_available():
+    args.device = "cuda"
+
+
+dir = os.getcwd() + "/datasets/RawData/%s" % (args.dataset)
 
 
 def get_data(comm: MPI.Comm):
-    # test data for a server
+    # test data for a server    
     test_data_raw = {}
     test_data_input = []
     test_data_label = []
@@ -37,8 +70,7 @@ def get_data(comm: MPI.Comm):
                 test_data_input.append([data_input])
 
             for data_label in test_data_raw[idx]["user_data"][client]["y"]:
-                test_data_label.append(data_label)
-
+                test_data_label.append(data_label)        
     test_dataset = Dataset(
         torch.FloatTensor(test_data_input), torch.tensor(test_data_label)
     )
@@ -64,15 +96,8 @@ def get_data(comm: MPI.Comm):
                     torch.tensor(train_data_raw[idx]["user_data"][client]["y"]),
                 )
             )
-
-    data_sanity_check(train_datasets, test_dataset, num_channel, num_pixel)
+    
     return train_datasets, test_dataset
-
-
-def get_model(comm: MPI.Comm):
-    ## User-defined model
-    model = CNN(num_channel, num_classes, num_pixel)
-    return model
 
 
 def main():
@@ -80,34 +105,61 @@ def main():
     comm_rank = comm.Get_rank()
     comm_size = comm.Get_size()
 
+    # read default configuration
+    cfg = OmegaConf.structured(Config)
+
     ## Reproducibility
     torch.manual_seed(1)
     torch.backends.cudnn.deterministic = True
 
     start_time = time.time()
     train_datasets, test_dataset = get_data(comm)
-    num_clients = len(train_datasets)
-    model = get_model(comm)
-    loss_fn = torch.nn.CrossEntropyLoss()
+
+    if cfg.data_sanity == True:
+        data_sanity_check(
+            train_datasets, test_dataset, args.num_channel, args.num_pixel
+        )
+
+    num_clients = len(train_datasets)    
+    model = get_model(args)
+    loss_fn = torch.nn.CrossEntropyLoss()   
     print(
         "----------Loaded Datasets and Model----------Elapsed Time=",
         time.time() - start_time,
     )
 
-    # read default configuration
-    cfg = OmegaConf.structured(Config)
+    cfg.device = args.device  
+
+    ## clients
+    cfg.num_clients = args.num_clients
+    cfg.fed.args.optim = args.client_optimizer
+    cfg.fed.args.optim_args.lr = args.client_lr
+    cfg.fed.args.num_local_epochs = args.num_local_epochs
+
+    ## server
+    cfg.fed.servername = args.server
+    cfg.num_epochs = args.num_epochs
+
+    ## outputs
+
+    cfg.use_tensorboard = True
+
+    # Testing code to check the configuration
+    print(OmegaConf.to_yaml(cfg))
+    print(args)
+    print(num_clients)
 
     if comm_size > 1:
         if comm_rank == 0:
-            rm.run_server(cfg, comm, model, loss_fn, num_clients, test_dataset, DataSet_name)
+            rm.run_server(cfg, comm, model, loss_fn, num_clients, test_dataset, args.dataset)
         else:
             rm.run_client(cfg, comm, model, loss_fn, num_clients, train_datasets)
         print("------DONE------", comm_rank)
     else:
-        rs.run_serial(cfg, model, loss_fn, train_datasets, test_dataset, DataSet_name)
+        rs.run_serial(cfg, model, loss_fn, train_datasets, test_dataset, args.dataset)
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     main()
 
 
