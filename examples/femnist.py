4e73c484f531d76b20a7c5ce610c0388b17a3e50
Sang-il Yim <yim0331@gmail.com>
2023-03-22 15:17:57 -0500

    add argument parser. update get_data to read channel 3 input

--- examples/femnist.py
+++ examples/femnist.py
@@ -11,17 +11,51 @@ from models.cnn import *
 import appfl.run_serial as rs
 import appfl.run_mpi as rm
 from mpi4py import MPI
+from models.utils import get_model
 
-DataSet_name = "FEMNIST"
-num_channel = 1  # 1 if gray, 3 if color
-num_classes = 62  # number of the image classes
-num_pixel = 28  # image size = (num_pixel, num_pixel)
+import argparse
+
+from torch.utils.data import DataLoader
+
+""" read arguments """
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--device", type=str, default="cpu")
+
+## dataset
+parser.add_argument("--dataset", type=str, default="FEMNIST")
+parser.add_argument("--num_channel", type=int, default=1)
+parser.add_argument("--num_classes", type=int, default=62)
+parser.add_argument("--num_pixel", type=int, default=28)
+parser.add_argument("--model", type=str, default="CNN")
+
+## clients
+parser.add_argument("--num_clients", type=int, default=1)
+parser.add_argument("--client_optimizer", type=str, default="Adam")
+parser.add_argument("--client_lr", type=float, default=1e-3)
+parser.add_argument("--num_local_epochs", type=int, default=1)
+
+## server
+parser.add_argument("--server", type=str, default="ServerFedAvg")
+parser.add_argument("--num_epochs", type=int, default=2)
+
+parser.add_argument("--server_lr", type=float, required=False)
+parser.add_argument("--mparam_1", type=float, required=False)
+parser.add_argument("--mparam_2", type=float, required=False)
+parser.add_argument("--adapt_param", type=float, required=False)
+
+
+args = parser.parse_args()
+
+if torch.cuda.is_available():
+    args.device = "cuda"
+
 
 dir = os.getcwd() + "/datasets/RawData/%s" % (DataSet_name)
 
-
 def get_data(comm: MPI.Comm):
-    # test data for a server
+    # test data for a server    
     test_data_raw = {}
     test_data_input = []
     test_data_label = []
@@ -31,14 +65,35 @@ def get_data(comm: MPI.Comm):
 
         for client in test_data_raw[idx]["users"]:
 
-            for data_input in test_data_raw[idx]["user_data"][client]["x"]:
-                data_input = np.asarray(data_input)
-                data_input.resize(28, 28)
-                test_data_input.append([data_input])
+            if args.num_channel == 1:
+                for data_input in test_data_raw[idx]["user_data"][client]["x"]:
+                    data_input = np.asarray(data_input)                
+                    data_input.resize(args.num_pixel, args.num_pixel)
+                    test_data_input.append([data_input])
+            elif args.num_channel == 3:                
+                for data_input in test_data_raw[idx]["user_data"][client]["x"]:
+                    R = []
+                    G = []
+                    B = []
+                    data_input = np.asarray(data_input)
+                    for i in range(int(data_input.size/3)):
+                        R.append(data_input[i])
+                        G.append(data_input[i+1])
+                        B.append(data_input[i+2])
+                    R = np.asarray(R)
+                    G = np.asarray(R)
+                    B = np.asarray(R)
+                    R.resize(args.num_pixel, args.num_pixel)
+                    G.resize(args.num_pixel, args.num_pixel)
+                    B.resize(args.num_pixel, args.num_pixel)
+                    test_data_input.append([R,G,B])
+            else:
+                print("Not Supporting Channel")
+                break;
 
             for data_label in test_data_raw[idx]["user_data"][client]["y"]:
-                test_data_label.append(data_label)
-
+                test_data_label.append(data_label)        
+    
     test_dataset = Dataset(
         torch.FloatTensor(test_data_input), torch.tensor(test_data_label)
     )
@@ -46,33 +101,47 @@ def get_data(comm: MPI.Comm):
     # training data for multiple clients
     train_data_raw = {}
     train_datasets = []
+    train_data_input_resize = []   
     for idx in range(36):
         with open("%s/train/all_data_%s_niid_05_keep_0_train_9.json" % (dir, idx)) as f:
             train_data_raw[idx] = json.load(f)
 
-        for client in train_data_raw[idx]["users"]:
+        for client in train_data_raw[idx]["users"]:            
 
-            train_data_input_resize = []
-            for data_input in train_data_raw[idx]["user_data"][client]["x"]:
-                data_input = np.asarray(data_input)
-                data_input.resize(28, 28)
-                train_data_input_resize.append([data_input])
+            if args.num_channel == 1:
+                for data_input in train_data_raw[idx]["user_data"][client]["x"]:
+                    data_input = np.asarray(data_input)                
+                    data_input.resize(args.num_pixel, args.num_pixel)
+                    train_data_input_resize.append([data_input])
+            elif args.num_channel == 3:                
+                for data_input in train_data_raw[idx]["user_data"][client]["x"]:
+                    R = []
+                    G = []
+                    B = []
+                    data_input = np.asarray(data_input)
+                    for i in range(int(data_input.size/3)):
+                        R.append(data_input[i])
+                        G.append(data_input[i+1])
+                        B.append(data_input[i+2])
+                    R = np.asarray(R)
+                    G = np.asarray(R)
+                    B = np.asarray(R)
+                    R.resize(args.num_pixel, args.num_pixel)
+                    G.resize(args.num_pixel, args.num_pixel)
+                    B.resize(args.num_pixel, args.num_pixel)
+                    train_data_input_resize.append([R,G,B])
+            else:
+                print("Not Supporting Channel")
+                break;
 
-            train_datasets.append(
-                Dataset(
-                    torch.FloatTensor(train_data_input_resize),
-                    torch.tensor(train_data_raw[idx]["user_data"][client]["y"]),
-                )
+        train_datasets.append(
+            Dataset(
+                torch.FloatTensor(train_data_input_resize),
+                torch.tensor(train_data_raw[idx]["user_data"][client]["y"]),
             )
-
-    data_sanity_check(train_datasets, test_dataset, num_channel, num_pixel)
+        )
+    
     return train_datasets, test_dataset
-
-
-def get_model(comm: MPI.Comm):
-    ## User-defined model
-    model = CNN(num_channel, num_classes, num_pixel)
-    return model
 
 
 def main():
@@ -80,34 +149,79 @@ def main():
     comm_rank = comm.Get_rank()
     comm_size = comm.Get_size()
 
+    # read default configuration
+    cfg = OmegaConf.structured(Config)
+
     ## Reproducibility
     torch.manual_seed(1)
     torch.backends.cudnn.deterministic = True
 
     start_time = time.time()
     train_datasets, test_dataset = get_data(comm)
-    num_clients = len(train_datasets)
-    model = get_model(comm)
-    loss_fn = torch.nn.CrossEntropyLoss()
+
+    
+    data_sanity_check(
+        train_datasets, test_dataset, args.num_channel, args.num_pixel
+    )
+
+    num_clients = len(train_datasets)    
+    model = get_model(args)
+    loss_fn = torch.nn.CrossEntropyLoss()   
     print(
         "----------Loaded Datasets and Model----------Elapsed Time=",
         time.time() - start_time,
     )
 
-    # read default configuration
-    cfg = OmegaConf.structured(Config)
+    cfg.device = args.device  
+
+    ## clients
+    cfg.num_clients = args.num_clients
+    cfg.fed.args.optim = args.client_optimizer
+    cfg.fed.args.optim_args.lr = args.client_lr
+    cfg.fed.args.num_local_epochs = args.num_local_epochs
+
+    ## server
+    cfg.fed.servername = args.server
+    cfg.num_epochs = args.num_epochs
+
+    ## outputs
+
+    cfg.use_tensorboard = True
+
+    # Testing code to check the configuration
+    print(OmegaConf.to_yaml(cfg))
+    print(args)
+    print(num_clients)
+    
+
+    print("///////////////////////////////////")
+    print(len(train_datasets[0]))
+
+    dl = DataLoader(
+        train_datasets[0],
+        num_workers=cfg.num_workers,
+        batch_size=len(train_datasets[0]),
+        shuffle=cfg.train_data_shuffle,
+        pin_memory=True,
+    )
+    for data, target in dl:
+        print(len(data[0]))
+        print(data[0])
+        print(target)    
+        break;
+    print("///////////////////////////////////")
 
     if comm_size > 1:
         if comm_rank == 0:
-            rm.run_server(cfg, comm, model, loss_fn, num_clients, test_dataset, DataSet_name)
+            rm.run_server(cfg, comm, model, loss_fn, num_clients, test_dataset, args.dataset)
         else:
             rm.run_client(cfg, comm, model, loss_fn, num_clients, train_datasets)
         print("------DONE------", comm_rank)
     else:
-        rs.run_serial(cfg, model, loss_fn, train_datasets, test_dataset, DataSet_name)
+        rs.run_serial(cfg, model, loss_fn, train_datasets, test_dataset, args.dataset)
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     main()
 
 
