395a9943ea1c4063d234a63578e40c18308797e9
Zilinghan <zl52@illinois.edu>
2023-06-05 11:55:07 -0700

    update client class numbers

--- examples/dataloader/partition_noiid.py
+++ examples/dataloader/partition_noiid.py
@@ -35,8 +35,8 @@ def partition_noiid(comm: MPI.Comm, cfg: DictConfig, dataset: str, seed: int = 4
     test_dataset = Dataset(torch.FloatTensor(test_data_input), torch.tensor(test_data_label))
 
     # training data for multiple clients
-    Cmin = {1: 10, 2: 7, 3: 6, 4: 5, 5: 5, 6: 4, 7: 4, 'none': 3}       # minimum sample classes for each client
-    Cmax = {1: 10, 2: 8, 3: 8, 4: 7, 5: 6, 6: 6, 7: 5, 'none': 5}       # maximum sample classes for each client
+    Cmin = {1: 10, 2: 7, 3: 6, 4: 5, 5: 5, 6: 4, 7: 4, 'none': 2}       # minimum sample classes for each client
+    Cmax = {1: 10, 2: 8, 3: 8, 4: 7, 5: 6, 6: 6, 7: 5, 'none': 4}       # maximum sample classes for each client
     train_data_raw = eval("torchvision.datasets." + dataset)(dir, download=False, train=True, transform=train_transform(dataset))
 
     # Split the dataset by label
