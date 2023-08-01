53da574a701af312eda126daf324633d119bed98
Zilinghan <zl52@illinois.edu>
2023-08-01 10:43:07 -0700

    change download to True to avoid dataset not found

--- tests/test_mnist.py
+++ tests/test_mnist.py
@@ -50,31 +50,9 @@ class CNN(nn.Module):
 
 
 def process_data(num_clients):
-    # Let's download the data first if data does not exist.
-    currentpath = os.getcwd()
-    datafolderpath = os.path.join(currentpath, "_data")
-    existsflag = False
-    if(os.path.exists(datafolderpath) and os.path.isdir(datafolderpath)):
-        mnistfolderpath = os.path.join(datafolderpath, "MNIST")
-        if(os.path.exists(mnistfolderpath) and os.path.isdir(mnistfolderpath)):
-            existsflag = True
-
-    if(not existsflag):
-        comm = MPI.COMM_WORLD
-        comm_size = comm.Get_size()
-        if comm_size > 1:
-            comm_rank = comm.Get_rank()
-            if comm_rank == 0:
-                print("Download by rank0")            
-                torchvision.datasets.MNIST("./_data", download=True, train=False, transform=ToTensor())
-            comm.Barrier()
-        else:
-            print("Download by serial")
-            torchvision.datasets.MNIST("./_data", download=True, train=False, transform=ToTensor())
-
     # test data for a server
     test_data_raw = torchvision.datasets.MNIST(
-        "./_data", download=False, train=False, transform=ToTensor()
+        "./_data", download=True, train=False, transform=ToTensor()
     )
 
     test_data_input = []
@@ -89,7 +67,7 @@ def process_data(num_clients):
 
     # training data for multiple clients
     train_data_raw = torchvision.datasets.MNIST(
-        "./_data", download=False, train=True, transform=ToTensor()
+        "./_data", download=True, train=True, transform=ToTensor()
     )
 
     split_train_data_raw = np.array_split(range(len(train_data_raw)), num_clients)
