6f02669f95f3ce916ec5a3029ed7226b4e6a7c30
Zilinghan <zl52@illinois.edu>
2023-08-01 09:41:32 -0700

    try resolving data not found issue in test

--- tests/test_mnist.py
+++ tests/test_mnist.py
@@ -50,6 +50,27 @@ class CNN(nn.Module):
 
 
 def process_data(num_clients):
+    # Let's download the data first if data does not exist.
+    currentpath = os.getcwd()
+    datafolderpath = os.path.join(currentpath, "_data")
+    existsflag = False
+    if(os.path.exists(datafolderpath) and os.path.isdir(datafolderpath)):
+        mnistfolderpath = os.path.join(datafolderpath, "MNIST")
+        if(os.path.exists(mnistfolderpath) and os.path.isdir(mnistfolderpath)):
+            existsflag = True
+
+    if(not existsflag):
+        comm = MPI.COMM_WORLD
+        comm_size = comm.Get_size()
+        if comm_size > 1:
+            comm_rank = comm.Get_rank()
+            if comm_rank == 0:
+                print("Download by rank0")            
+                torchvision.datasets.MNIST("./_data", download=True, train=False, transform=ToTensor())
+            comm.Barrier()
+        else:
+            print("Download by serial")
+            torchvision.datasets.MNIST("./_data", download=True, train=False, transform=ToTensor())
 
     # test data for a server
     test_data_raw = torchvision.datasets.MNIST(
@@ -89,30 +110,6 @@ def process_data(num_clients):
         )
 
     return train_datasets, test_dataset
-
-
-# Let's download the data first if data does not exist.
-currentpath = os.getcwd()
-datafolderpath = os.path.join(currentpath, "_data")
-existsflag = False
-if(os.path.exists(datafolderpath) and os.path.isdir(datafolderpath)):
-    mnistfolderpath = os.path.join(datafolderpath, "MNIST")
-    if(os.path.exists(mnistfolderpath) and os.path.isdir(mnistfolderpath)):
-        existsflag = True
-
-if(not existsflag):
-    comm = MPI.COMM_WORLD
-    comm_size = comm.Get_size()
-    if comm_size > 1:
-        comm_rank = comm.Get_rank()
-        if comm_rank == 0:
-            print("Download by rank0")            
-            torchvision.datasets.MNIST("./_data", download=True, train=False, transform=ToTensor())
-        comm.Barrier()
-    else:
-        print("Download by serial")
-        torchvision.datasets.MNIST("./_data", download=True, train=False, transform=ToTensor())
-    
 
 
 def test_mnist_fedavg():
