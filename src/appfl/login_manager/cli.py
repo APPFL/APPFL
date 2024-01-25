3859f8b73209dccb79c6bde23490a80288afcc6d
Zilinghan <zl52@illinois.edu>
2024-01-25 13:43:12 -0600

    reformat & fix account change

--- src/appfl/login_manager/cli.py
+++ src/appfl/login_manager/cli.py
@@ -1,20 +1,22 @@
 from appfl.login_manager import LoginManager
 
 def auth():
+    prompt = "Please select whether you are authenticating for the federated learning server or client (Enter 1 or 2)"
     while True:
         mode = input(
-            "Please select whether you are authenticating for the federated learning server or client (Enter 1 or 2)\n"
-            "-------------------------------------------------------------------------------------------------------\n"
+            f"{prompt}\n"
+            f"{'-'*len(prompt)}\n"
             "1. Federated learning server\n"
             "2. Federated learning client\n"
-            "-------------------------------------------------------------------------------------------------------\n"
+            f"{'-'*len(prompt)}\n"
         )
         if mode in ("1", "2"):
+            f"{'-'*len(prompt)}\n"
             break
         else:
             print(
                 "Invalid input, please try again\n"
-                "-------------------------------------------------------------------------------------------------------\n"
+                f"{'-'*len(prompt)}\n"
             )
     is_fl_server = mode == "1"
     login_manager = LoginManager(is_fl_server=is_fl_server)
@@ -22,19 +24,28 @@ def auth():
         action = input(
             f"You have already logged in as a federated learning {'server' if is_fl_server else 'client'}\n"
             "You can either logout (1), change to another account (2), or just exit (3)\n"
+            f"{'-'*len(prompt)}\n"
             "1. Logout\n"
             "2. Change to another account\n"
             "3. Exit\n"
+            f"{'-'*len(prompt)}\n"
         )
         if not action in ["1", "2", "3"]:
             print("Invalid input, exiting...")
             return
         if action == "1":
             login_manager.logout()
+            print("Successfully logged out\n")
+            return
         elif action == "2":
             login_manager.logout()
-            login_manager = LoginManager(is_fl_server=is_fl_server)
+            login_manager.ensure_logged_in()
+            print(
+                f"{'-'*len(prompt)}\n"
+                f"Successfully logged in as a federated learning {'server' if is_fl_server else 'client'}\n"
+            )
     else:
         print(
+            f"{'-'*len(prompt)}\n"
             f"Successfully logged in as a federated learning {'server' if is_fl_server else 'client'}\n"
         )
