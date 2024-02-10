ae81a540e3dee2f562df5fe2b662dd252749b99c
Zilinghan <zl52@illinois.edu>
2024-02-10 13:28:01 -0600

    add cmts for auth token provider and interceptor

--- src/appfl/communicator/grpc/authenticator.py
+++ src/appfl/communicator/grpc/authenticator.py
@@ -2,10 +2,16 @@ import grpc
 from appfl.login_manager import BaseAuthenticator
 
 class APPFLAuthMetadataProvider(grpc.AuthMetadataPlugin):
+    """
+    `APPFLAuthMetadataProvider` is used to attach authentication tokens to the metadata of each RPC.
+    """
     def __init__(self, authenticator: BaseAuthenticator):
         self.authenticator = authenticator
 
     def __call__(self, context, callback):
+        """
+        Attach tokens from `authenticator.get_auth_token()` to the metadata of the RPC.
+        """
         metadata = []
         auth_tokens = self.authenticator.get_auth_token()
         for key, value in auth_tokens.items():
@@ -14,6 +20,9 @@ class APPFLAuthMetadataProvider(grpc.AuthMetadataPlugin):
         callback(metadata, None)
 
 class APPFLAuthMetadataInterceptor(grpc.ServerInterceptor):
+    """
+    `APPFLAuthMetadataInterceptor` is used to intercept the metadata of each RPC and validate the authentication tokens.
+    """
     def __init__(self, authenticator: BaseAuthenticator):
         self.authenticator = authenticator
         def abort(ignored_request, context):
@@ -22,7 +31,7 @@ class APPFLAuthMetadataInterceptor(grpc.ServerInterceptor):
         self._abortion = grpc.unary_unary_rpc_method_handler(abort)
 
     def intercept_service(self, continuation, handler_call_details):
-        
+        """Validate the authentication tokens in the metadata of the RPC using `authenticator.validate_auth_token()`, and abort the RPC if the tokens are invalid."""
         metadata = dict(handler_call_details.invocation_metadata)
         valid = self.authenticator.validate_auth_token(metadata)
         if not valid:
