03d6e3bb1a5a3229b93dd39bfa373916d355686b
Zilinghan <zl52@illinois.edu>
2024-02-17 23:32:28 -0600

    utils to load func/class from source

--- src/appfl/misc/utils.py
+++ src/appfl/misc/utils.py
@@ -1,15 +1,16 @@
-import torch
 import os
-from omegaconf import DictConfig
-import logging
-import random
-import numpy as np
-import copy
-import os.path as osp
-import pickle as pkl
-import string
-import importlib.util
 import sys
+import copy
+import torch
+import random
+import string
+import pathlib
+import logging
+import numpy as np
+import pickle as pkl
+import os.path as osp
+import importlib.util
+from omegaconf import DictConfig
 
 def validation(self, dataloader, metric):
     if self.loss_fn is None or dataloader is None:
@@ -348,3 +349,49 @@ def run_function_from_file(file_path, function_name, *args, **kwargs):
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
+    
+def create_instance_from_file_source(source, class_name, *args, **kwargs):
+    """
+    Creates an instance of a class from a given source code.
+
+    :param source: The source code where the class is defined.
+    :param class_name: The name of the class to be instantiated.
+    :param args: Positional arguments to be passed to the class constructor
+    """
+    # Create a temporary file to store the source code
+    _home = pathlib.Path.home()
+    dirname = osp.join(_home, ".appfl", "tmp")
+    file_path = osp.join(dirname, f"{id_generator()}.py")
+    with open(file_path, "w") as file:
+        file.write(source)
+
+    # Create an instance from the temporary file
+    instance = create_instance_from_file(file_path, class_name, *args, **kwargs)
+
+    # Remove the temporary file
+    os.remove(file_path)
+
+    return instance
+
+def get_function_from_file_source(source, function_name):
+    """
+    Gets a function from a given source code.
+
+    :param source: The source code where the function is defined.
+    :param function_name: The name of the function to be retrieved.
+    :return: The function object, or None if retrieval fails.
+    """
+    # Create a temporary file to store the source code
+    _home = pathlib.Path.home()
+    dirname = osp.join(_home, ".appfl", "tmp")
+    file_path = osp.join(dirname, f"{id_generator()}.py")
+    with open(file_path, "w") as file:
+        file.write(source)
+
+    # Get the function from the temporary file
+    function = get_function_from_file(file_path, function_name)
+
+    # Remove the temporary file
+    os.remove(file_path)
+
+    return function
