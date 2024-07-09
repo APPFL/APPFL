4a54c97ef487599a41f15effcbd3c496eba87b49
zilinghan <zl52@illinois.edu>
2024-07-09 13:23:30 -0500

    [benchmark-grpc-proxystore]: add configurations and client launch scripts

--- examples/bench/scripts/grpc_s3_serial.sh
+++ examples/bench/scripts/grpc_s3_serial.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --mem=240g
+#SBATCH --mem=240g 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --partition=cpu
 #SBATCH --account=bcwo-delta-cpu                    
 #SBATCH --job-name=grpc-128-clients    
-#SBATCH --time=00:06:00                        
+#SBATCH --time=00:10:00                        
 
 source ~/.bashrc
 conda activate appfl
 cd /projects/bcdz/zl52/APPFL/examples/bench
-python run_grpc_clients_serial.py --num_clients 128 --server_uri 141.142.144.162:50051
+python run_grpc_clients_serial.py --num_clients 8 --client_config configs/communication/client_grpcs3.yaml --server_uri 141.142.144.162:50051
