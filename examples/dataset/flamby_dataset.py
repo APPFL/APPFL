17efc1d7160e7ff6fff53b60f01e834a533e18d9
Zilinghan <zl52@illinois.edu>
2024-07-31 19:41:00 -0500

    add privacy test exps on FLamby

--- examples/dataset/flamby_dataset.py
+++ examples/dataset/flamby_dataset.py
@@ -9,34 +9,35 @@ except:
 
 def get_flamby(
     dataset: str, 
-    num_clients: int,
-    client_id: int,
+    num_clients: int = 1,
+    client_id: int = 0,
+    test_only: bool = False,
     **kwargs,
 ):
     if dataset == 'TcgaBrca':
         assert num_clients <= 6, "TcgaBrca dataset can support at most six clients"
         test_dataset =  FedTcgaBrca(train=False, pooled=True)
         train_dataset = FedTcgaBrca(train=True, center=client_id, pooled=False)
-        return train_dataset, test_dataset
+        return (train_dataset, test_dataset) if not test_only else test_dataset
     elif dataset == 'HeartDisease':
         assert num_clients <= 4, "Heart disease dataset can support at most four clients"
         test_dataset =  FedHeartDisease(train=False, pooled=True)
         train_dataset = FedHeartDisease(train=True, center=client_id, pooled=False)
-        return train_dataset, test_dataset
+        return (train_dataset, test_dataset) if not test_only else test_dataset
     elif dataset == 'IXI':
         assert num_clients <= 3, "IXI dataset can support at most three clients"
         test_dataset = FedIXITiny(train=False, pooled=True)
         train_dataset = FedIXITiny(train=True, center=client_id, pooled=False) 
-        return train_dataset, test_dataset
+        return (train_dataset, test_dataset) if not test_only else test_dataset
     elif dataset == 'ISIC2019':
         assert num_clients <= 6, "ISIC 2019 dataset can support at most six clients"
         test_dataset = FedIsic2019(train=False, pooled=True)
         train_dataset = FedIsic2019(train=True, center=client_id, pooled=False)
-        return train_dataset, test_dataset
+        return (train_dataset, test_dataset) if not test_only else test_dataset
     elif dataset == 'Kits19':
         assert num_clients <= 6, "Kits19 dataset can support at most six clients"
         test_dataset = FedKits19(train=False, pooled=True)
         train_dataset = FedKits19(train=True, center=client_id, pooled=False) 
-        return train_dataset, test_dataset
+        return (train_dataset, test_dataset) if not test_only else test_dataset
     else:
         raise NotImplementedError    
