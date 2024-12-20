abb0e76df1ca8e7556f58c317b6fb31ab63fcf9b
Carlyn Lee <Carlyn.Lee@gmail.com>
2024-12-19 23:40:55 -0800

    dynamic LD paths

---
+++ resources/model/fc.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+
+
+class FC(nn.Module):
+    """
+    A Fully connected layer.
+    """
+
+    def __init__(self, input_size):
+        super().__init__()
+        self.fc = nn.Linear(input_size, 1)
+
+    def forward(self, x):
+        out = self.fc(x)
+        return out
