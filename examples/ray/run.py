b6d79bc39ca3184042b41f1802a6902230e4c0eb
Aditya Sinha <aditya47@illinois.edu>
2025-02-25 21:51:00 -0600

    Updated code by comment, added docs - Added tutorial doc for ray - updated ray cluster config to use publically available AMI - added s3 support with ray

--- examples/ray/run.py
+++ examples/ray/run.py
@@ -9,10 +9,10 @@ argparser = argparse.ArgumentParser()
 argparser.add_argument(
     "--server_config",
     type=str,
-    default="./resources/config_gc/mnist/server_fedcompass.yaml",
+    default="./resources/config_ray/mnist/server_fedcompass.yaml",
 )
 argparser.add_argument(
-    "--client_config", type=str, default="./resources/config_gc/mnist/clients.yaml"
+    "--client_config", type=str, default="./resources/config_ray/mnist/clients.yaml"
 )
 args = argparser.parse_args()
 
@@ -33,8 +33,8 @@ server_communicator = RayServerCommunicator(
 # Get sample size from clients
 server_communicator.send_task_to_all_clients(task_name="get_sample_size")
 sample_size_ret = server_communicator.recv_result_from_all_clients()[1]
-for client_endpoint_id, sample_size in sample_size_ret.items():
-    server_agent.set_sample_size(client_endpoint_id, sample_size["sample_size"])
+for client_id, sample_size in sample_size_ret.items():
+    server_agent.set_sample_size(client_id, sample_size["sample_size"])
 
 if (
     hasattr(server_agent_config.client_configs, "data_readiness_configs")
@@ -48,13 +48,13 @@ if (
     readiness_reports = server_communicator.recv_result_from_all_clients()[1]
     # Restructure the data to match the function's expected input
     restructured_report = {}
-    for client_endpoint_id, client_report in readiness_reports.items():
+    for client_id, client_report in readiness_reports.items():
         # Assuming 'data_readiness' is the key containing the actual report
         client_data = client_report.get("data_readiness", {})
         for attribute, value in client_data.items():
             if attribute not in restructured_report:
                 restructured_report[attribute] = {}
-            restructured_report[attribute][client_endpoint_id] = value
+            restructured_report[attribute][client_id] = value
     # Handle 'plots' separately if they exist
     if "plots" in restructured_report:
         plot_data = restructured_report.pop("plots")
@@ -75,31 +75,31 @@ server_communicator.send_task_to_all_clients(
 model_futures = {}
 client_rounds = {}
 while not server_agent.training_finished():
-    client_endpoint_id, client_model, client_metadata = (
+    client_id, client_model, client_metadata = (
         server_communicator.recv_result_from_one_client()
     )
     server_agent.logger.info(
-        f"Received model from client {client_endpoint_id}, with metadata:\n{pprint.pformat(client_metadata)}"
+        f"Received model from client {client_id}, with metadata:\n{pprint.pformat(client_metadata)}"
     )
     global_model = server_agent.global_update(
-        client_endpoint_id,
+        client_id,
         client_model,
         **client_metadata,
     )
     if isinstance(global_model, Future):
-        model_futures[client_endpoint_id] = global_model
+        model_futures[client_id] = global_model
     else:
         if isinstance(global_model, tuple):
             global_model, metadata = global_model
         else:
             metadata = {}
-        if client_endpoint_id not in client_rounds:
-            client_rounds[client_endpoint_id] = 0
-        client_rounds[client_endpoint_id] += 1
-        metadata["round"] = client_rounds[client_endpoint_id]
+        if client_id not in client_rounds:
+            client_rounds[client_id] = 0
+        client_rounds[client_id] += 1
+        metadata["round"] = client_rounds[client_id]
         if not server_agent.training_finished():
             server_communicator.send_task_to_one_client(
-                client_endpoint_id,
+                client_id,
                 task_name="train",
                 model=global_model,
                 metadata=metadata,
@@ -107,26 +107,26 @@ while not server_agent.training_finished():
             )
     # Deal with the model futures
     del_keys = []
-    for client_endpoint_id in model_futures:
-        if model_futures[client_endpoint_id].done():
-            global_model = model_futures[client_endpoint_id].result()
+    for client_id in model_futures:
+        if model_futures[client_id].done():
+            global_model = model_futures[client_id].result()
             if isinstance(global_model, tuple):
                 global_model, metadata = global_model
             else:
                 metadata = {}
-            if client_endpoint_id not in client_rounds:
-                client_rounds[client_endpoint_id] = 0
-            client_rounds[client_endpoint_id] += 1
-            metadata["round"] = client_rounds[client_endpoint_id]
+            if client_id not in client_rounds:
+                client_rounds[client_id] = 0
+            client_rounds[client_id] += 1
+            metadata["round"] = client_rounds[client_id]
             if not server_agent.training_finished():
                 server_communicator.send_task_to_one_client(
-                    client_endpoint_id,
+                    client_id,
                     task_name="train",
                     model=global_model,
                     metadata=metadata,
                     need_model_response=True,
                 )
-            del_keys.append(client_endpoint_id)
+            del_keys.append(client_id)
     for key in del_keys:
         model_futures.pop(key)
 
