1f6d58c104ab0afc33e9396ebc419c284546b366
Yijiang Li <li.yijiang.0510@gmail.com>
2025-03-10 15:39:34 -0500

    additional time breakdown

--- src/appfl/comm/globus_compute/utils/executor.py
+++ src/appfl/comm/globus_compute/utils/executor.py
@@ -1,3 +1,4 @@
+import time
 from appfl.agent import ClientAgent
 from appfl.comm.globus_compute.utils.client_utils import (
     load_global_model,
@@ -8,9 +9,27 @@ from appfl.comm.globus_compute.utils.client_utils import (
 def get_sample_size_executor(
     client_agent_config=None,
     **kwargs,
-):
+):  
+    task_sent_time = float(client_agent_config.start_time)
+    total_task_sent_time = time.time() - task_sent_time
+    print(f"serve to client time: {total_task_sent_time}")
+    task_execution_start_time = time.time()
     client_agent = ClientAgent(client_agent_config=client_agent_config)
-    return None, {"sample_size": client_agent.get_sample_size()}
+    sample_size = client_agent.get_sample_size()
+    # return None, {"sample_size": client_agent.get_sample_size()}
+    total_task_execution_time = time.time() - task_execution_start_time
+    print(f"total get sample time: {total_task_execution_time}")
+    return None, {
+        "sample_size": sample_size, 
+        "end_time": time.time(), 
+        "server_to_client_time": total_task_sent_time,
+        "total_model_download_time": "n/a",
+        "total_execution_time": total_task_execution_time,
+        "total_pre_val_time": "n/a",
+        "total_forward_time": "n/a",
+        "total_backward_time": "n/a",
+        "total_val_time": "n/a",
+    }
 
 
 def data_readiness_report_executor(
@@ -28,10 +47,23 @@ def train_executor(
     model=None,
     meta_data=None,
 ):
-    client_agent = ClientAgent(client_agent_config=client_agent_config)
+    # client_agent = ClientAgent(client_agent_config=client_agent_config)
+    task_sent_time = float(client_agent_config.start_time)
+    total_task_sent_time = time.time() - task_sent_time
+    print(f"serve to client time: {total_task_sent_time}")
+    
+    donwload_model_start_time = time.time()
     if model is not None:
-        model = load_global_model(client_agent.client_agent_config, model)
-        client_agent.load_parameters(model)
+        # model = load_global_model(client_agent.client_agent_config, model)
+        # client_agent.load_parameters(model)
+        model = load_global_model(client_agent_config, model)
+    total_model_download_time = time.time() - donwload_model_start_time
+    print(f"total donwload model time: {total_model_download_time}")
+    
+    training_start_time = time.time()  
+    client_agent = ClientAgent(client_agent_config=client_agent_config)
+    client_agent.load_parameters(model)
+
     client_agent.train(**meta_data)
     local_model = client_agent.get_parameters()
     if isinstance(local_model, tuple):
@@ -44,4 +76,14 @@ def train_executor(
         meta_data["local_model_key"] if "local_model_key" in meta_data else None,
         meta_data["local_model_url"] if "local_model_url" in meta_data else None,
     )
+    total_task_execution_time = time.time() - training_start_time
+
+    meta_data_local['end_time'] = time.time()
+    meta_data_local['server_to_client_time'] = total_task_sent_time
+    meta_data_local['total_model_download_time'] = total_model_download_time
+    meta_data_local['total_execution_time'] = total_task_execution_time
+    meta_data_local['total_pre_val_time'] = client_agent.trainer.total_pre_val_time
+    meta_data_local['total_forward_time'] = client_agent.trainer.total_forward_time
+    meta_data_local['total_backward_time'] = client_agent.trainer.total_backward_time
+    meta_data_local['total_val_time'] = client_agent.trainer.total_val_time
     return local_model, meta_data_local
