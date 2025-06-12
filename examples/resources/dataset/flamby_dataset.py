7be63a9241e9e6604d488403fec2989c11b3b35c
Zilinghan <zl52@illinois.edu>
2025-06-11 20:50:47 -0500

    update dataset

--- examples/resources/dataset/flamby_dataset.py
+++ examples/resources/dataset/flamby_dataset.py
@@ -1,13 +1,3 @@
-try:
-    from flamby.datasets.fed_tcga_brca import FedTcgaBrca
-    from flamby.datasets.fed_heart_disease import FedHeartDisease
-    from flamby.datasets.fed_ixi import FedIXITiny
-    from flamby.datasets.fed_isic2019 import FedIsic2019
-    from flamby.datasets.fed_kits19 import FedKits19
-except:  # noqa E722
-    pass
-
-
 def get_flamby(
     dataset: str,
     num_clients: int,
@@ -15,11 +5,13 @@ def get_flamby(
     **kwargs,
 ):
     if dataset == "TcgaBrca":
+        from flamby.datasets.fed_tcga_brca import FedTcgaBrca
         assert num_clients <= 6, "TcgaBrca dataset can support at most six clients"
         test_dataset = FedTcgaBrca(train=False, pooled=True)
         train_dataset = FedTcgaBrca(train=True, center=client_id, pooled=False)
         return train_dataset, test_dataset
     elif dataset == "HeartDisease":
+        from flamby.datasets.fed_heart_disease import FedHeartDisease
         assert num_clients <= 4, (
             "Heart disease dataset can support at most four clients"
         )
@@ -27,16 +19,19 @@ def get_flamby(
         train_dataset = FedHeartDisease(train=True, center=client_id, pooled=False)
         return train_dataset, test_dataset
     elif dataset == "IXI":
+        from flamby.datasets.fed_ixi import FedIXITiny
         assert num_clients <= 3, "IXI dataset can support at most three clients"
         test_dataset = FedIXITiny(train=False, pooled=True)
         train_dataset = FedIXITiny(train=True, center=client_id, pooled=False)
         return train_dataset, test_dataset
     elif dataset == "ISIC2019":
+        from flamby.datasets.fed_isic2019 import FedIsic2019
         assert num_clients <= 6, "ISIC 2019 dataset can support at most six clients"
         test_dataset = FedIsic2019(train=False, pooled=True)
         train_dataset = FedIsic2019(train=True, center=client_id, pooled=False)
         return train_dataset, test_dataset
     elif dataset == "Kits19":
+        from flamby.datasets.fed_kits19 import FedKits19
         assert num_clients <= 6, "Kits19 dataset can support at most six clients"
         test_dataset = FedKits19(train=False, pooled=True)
         train_dataset = FedKits19(train=True, center=client_id, pooled=False)
