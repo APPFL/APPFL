01bbcb6c0859ae45808e26ef0f0fc1f7f35ea315
Kaveen Hiniduma <hiniduma.1@buckeyemail.osu.edu>
2025-06-26 23:47:48 -0400

    updated notebooks

--- examples/resources/dataset/flamby_dataset.py
+++ examples/resources/dataset/flamby_dataset.py
@@ -1,3 +1,4 @@
+from appfl.misc.data_readiness.data_pollute import *
 def get_flamby(
     dataset: str,
     num_clients: int,
@@ -26,6 +27,10 @@ def get_flamby(
         assert num_clients <= 3, "IXI dataset can support at most three clients"
         test_dataset = FedIXITiny(train=False, pooled=True)
         train_dataset = FedIXITiny(train=True, center=client_id, pooled=False)
+        train_dataset = add_noise_to_subset(
+            train_dataset, scale=2, fraction=0.7
+        )
+
         return train_dataset, test_dataset
     elif dataset == "ISIC2019":
         from flamby.datasets.fed_isic2019 import FedIsic2019
---
+++ examples/resources/dataset/flamby_ixi_polluted.py
@@ -0,0 +1,20 @@
+from appfl.misc.data_readiness.data_pollute import *
+def get_flamby_ixi_polluted(
+    dataset: str,
+    num_clients: int,
+    client_id: int,
+    **kwargs,
+):
+    if dataset == "IXI":
+        from flamby.datasets.fed_ixi import FedIXITiny
+
+        assert num_clients <= 3, "IXI dataset can support at most three clients"
+        test_dataset = FedIXITiny(train=False, pooled=True)
+        train_dataset = FedIXITiny(train=True, center=client_id, pooled=False)
+        train_dataset = add_noise_to_subset(
+            train_dataset, scale=2, fraction=0.7
+        )
+
+        return train_dataset, test_dataset
+    else:
+        raise NotImplementedError
