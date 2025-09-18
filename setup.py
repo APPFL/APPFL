dc14b1a0c32979be9f6aa7209f49c9891bf54bb3
Zilinghan <zl52@illinois.edu>
2025-09-18 12:03:17 -0500

    release version 1.8.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ else:
 
 setuptools.setup(
     name="appfl",
-    version="1.7.0",
+    version="1.8.0",
     author=AUTHOR,
     description="An open-source package for privacy-preserving federated learning",
     long_description=long_description,
