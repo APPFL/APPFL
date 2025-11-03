e2adcc3bec4d0eaba74739cafc9e1a8b0dd2be4c
Zilinghan <zl52@illinois.edu>
2025-11-03 11:28:50 -0600

    test rejecting v1.7.0

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ else:
 
 setuptools.setup(
     name="appfl",
-    version="1.8.0",
+    version="1.7.0",
     author=AUTHOR,
     description="An open-source package for privacy-preserving federated learning",
     long_description=long_description,
